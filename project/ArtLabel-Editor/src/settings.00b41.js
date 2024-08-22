c8b4a819d3ff87cc0ff09ab70ea4c242ea39d3be
cooper <cooper.huang@kinghub.cc>
2024-08-22 15:02:22 +0800

    add: ArtLabel-Editor

---
+++ project/ArtLabel-Editor/src/settings.00b41.js
@@ -0,0 +1,28 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "ui"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/scene/ArtLabelEditor.fire",
+    orientation: "",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "a680c",
+        main: "5c7cb"
+    }
+};
